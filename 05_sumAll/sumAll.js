080a213e9f0dc4fe2688256e0dc2162babae82fc
mekoness <mekoness@gmail.com>
2022-10-09 21:11:11 -0700

    Solutions Part 1

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
-
+const sumAll = function(...arr) {
+    let sum = 0
+    if (arr[0] < 0 || arr[1] < 0) {
+        return "ERROR"
+    }
+    if (Number.isInteger(arr[0]) && Number.isInteger(arr[1])) {
+        if (arr[0] > arr[1]) {
+            arr.sort((a, b) => a - b)
+        }
+        for (let i = arr[0]; i <= arr[1]; i++) {
+            sum += i
+        }
+        return sum
+    } else {
+        return "ERROR"
+    }
+    
 };
 
 // Do not edit below this line
