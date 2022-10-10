d3a86b21c64b5faf5ba67879608d743fa03dc757
mekoness <mekoness@gmail.com>
2022-10-10 12:21:02 -0700

    Solutions part 2

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,18 @@
-const leapYears = function() {
+// const leapYears = function(year) {
+//     return (year % 100 === 0) ? (year % 400 === 0) : (year % 4 === 0);
+// };
 
+const leapYears = function(year) {
+    if (year % 4 === 0) {
+        if (year % 100 === 0 && year % 400 === 0) {
+            return true
+        } else if (year % 100 === 0) {
+            return false
+        }
+    } else {
+        return false
+    }
+    return true
 };
 
 // Do not edit below this line
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {
+  let tempC = ((temp - 32) * 5) / 9
+  return Math.round((tempC + Number.EPSILON) * 10) / 10
 };
 
-const ctof = function() {
-
+const ctof = function(temp) {
+  let tempF = (temp * 9)/5 + 32
+  return Math.round((tempF + Number.EPSILON) * 10) / 10
 };
 
 // Do not edit below this line
